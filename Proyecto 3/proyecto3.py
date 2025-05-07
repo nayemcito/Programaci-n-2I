f80c1483bca1008bae2279625d8f1f3fbfefa318
Nayaim <mauroshakay222@gmail.com>
2025-05-07 09:23:09 -0500

    mi primer commit

---
+++ Proyecto 3/proyecto3.py
@@ -0,0 +1,6 @@
+#proyecto 3: Tabla de multiplicar personalizada 
+x= int(input("Ingrese un número: "))
+print(f"Tabla de multiplicar del {x}: ")
+for i in range(1,13):
+    producto=i*x
+    print("producto:", producto)
