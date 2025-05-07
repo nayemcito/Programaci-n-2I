5ffba20709b52ac6f510af605b213903b08130d8
Nayaim <mauroshakay222@gmail.com>
2025-05-07 09:43:54 -0500

    mi primer commit

--- Proyecto 3/proyecto3.py
+++ Proyecto 3/proyecto3.py
@@ -3,4 +3,4 @@ x= int(input("Ingrese un número: "))
 print(f"Tabla de multiplicar del {x}: ")
 for i in range(1,13):
     producto=i*x
-    print("producto:", producto)
+    print(f"producto:",i , "x", x ,"=", producto)
